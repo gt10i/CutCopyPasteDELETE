3ea483c08ce1b5ae1361da6675385e7c7e1d97b7
George <gt10i.dev@gmail.com>
2024-09-25 20:02:02 +0100

    Achieved deleting text in the editor

--- extension.js
+++ extension.js
@@ -8,23 +8,44 @@ function activate(context) {
 
 	console.log('Congratulations, your extension "cutcopypastedelete" is now active!');
 
-	let disposable = vscode.commands.registerCommand('cutcopypastedelete.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('cutcopypastedelete.deleteEditorContextMenu', function () {
 
 		const editor = vscode.window.activeTextEditor;
-		const selection = editor.selection;
-		if (selection && !selection.isEmpty) {
-			const selectionRange = new vscode.Range(selection.start, selection.end);
-			const highlighted = editor.document.getText(selectionRange);
 
-		vscode.window.showInformationMessage('Hello World from CutCopyPasteDELETE!' + highlighted);
+		if (editor) {
+			const document = editor.document;
+			editor.edit(editBuilder => {
+				editor.selections.forEach(sel => {
+					const range = sel.isEmpty ? document.getWordRangeAtPosition(sel.start) || sel : sel;
+					let word = editor.document.getText(range);
+					editBuilder.delete(range)
+					vscode.window.showInformationMessage(range.start.character.toString() + word);
+				})
+			}) // apply the (accumulated) replacement(s) (if multiple cursors/selections)
 		}
 
+
+
+
+		// const editor = vscode.window.activeTextEditor;
+		// const selection = editor.selection;
+		// if (selection && !selection.isEmpty) {
+		// 	const selectionRange = new vscode.Range(selection.start, selection.end);
+		// 	const highlighted = editor.document.getText(selectionRange);
+		// 	const replaced = highlighted.replace(highlighted,"T");
+
+		// editor.edit(editBuilder => {
+		// 	editBuilder.replace(editor.document.getText(selectionRange), "textReplace");
+		//   }).catch(err => console.log(err));
+		// }
+
+		// vscode.window.showInformationMessage(replaced);
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,

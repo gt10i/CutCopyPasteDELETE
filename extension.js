644a67826e2b7d21fb73f27a2a48e18bf90c93f7
George <gt10i.dev@gmail.com>
2024-09-25 20:02:03 +0100

    Added deleting multiple selections. Does not delete lines on no selection

--- extension.js
+++ extension.js
@@ -6,43 +6,53 @@ const vscode = require('vscode');
 
 function activate(context) {
 
-	console.log('Congratulations, your extension "cutcopypastedelete" is now active!');
-
 	let disposable = vscode.commands.registerCommand('cutcopypastedelete.deleteEditorContextMenu', function () {
 
 		const editor = vscode.window.activeTextEditor;
 
 		if (editor) {
-			const document = editor.document;
+
+			// const selection = editor.selection;
+			// console.log(editor.document.getText(selection));
+
+			// if (selection && !selection.isEmpty) {
+
+			// 	editor.edit(editBuilder => {
+			// 		editBuilder.delete(selection)
+
+			// 	}).catch(err => console.log(err));
+			// }
+
 			editor.edit(editBuilder => {
-				editor.selections.forEach(sel => {
-					const range = sel.isEmpty ? document.getWordRangeAtPosition(sel.start) || sel : sel;
-					let word = editor.document.getText(range);
-					editBuilder.delete(range)
-					vscode.window.showInformationMessage(range.start.character.toString() + word);
+				editor.selections.forEach(selection => {
+					if (selection && !selection.isEmpty) {
+						console.log(selection);
+						editBuilder.delete(selection)
+					}
 				})
-			}) // apply the (accumulated) replacement(s) (if multiple cursors/selections)
+			})
+
+			// delete line - Make this an option?
+			
+			console.log("End");
+			vscode.window.showInformationMessage("End");
 		}
-
-
-
-
-		// const editor = vscode.window.activeTextEditor;
-		// const selection = editor.selection;
-		// if (selection && !selection.isEmpty) {
-		// 	const selectionRange = new vscode.Range(selection.start, selection.end);
-		// 	const highlighted = editor.document.getText(selectionRange);
-		// 	const replaced = highlighted.replace(highlighted,"T");
-
-		// editor.edit(editBuilder => {
-		// 	editBuilder.replace(editor.document.getText(selectionRange), "textReplace");
-		//   }).catch(err => console.log(err));
-		// }
-
-		// vscode.window.showInformationMessage(replaced);
 	});
 
-	context.subscriptions.push(disposable);
+		// const selectionRange = new vscode.Range(selection.start, selection.end);
+		// const selectedText = editor.document.getText(selectionRange);
+		// console.log(selectedText);
+
+	// const document = editor.document;
+
+	// editor.edit(editBuilder => {
+	// 	editor.selections.forEach(sel => {
+	// 		const range = sel.isEmpty ? document.getWordRangeAtPosition(sel.start) || sel : sel;
+	// 	})
+	// })
+
+
+context.subscriptions.push(disposable);
 }
 
 function deactivate() { }
